1dc7fd22793ba66e25c4faf0041992beba6c631e
Kipras Melnikovas <kipras@kipras.org>
2019-04-13 23:28:40 +0300

    Add dashboard comp ts - dynamic hero selection 🔥
    This took some good work & researching on duckduckgo / stackoverflow
    hehe.
    
    This also saves your selection into localStorage so that your selection
    will be remembered the next time you come back.
    
    By the way, the <ng-template>s will soon be fixed inside the html
    - I thought that's the only way to do them, but I forgot I can use
    inline javascript.
    
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
+import { isNumber, isString } from "util";
 
 @Component({
 	selector: "app-dashboard",
@@ -6,7 +9,41 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
-	constructor() {}
+	originalHeroes: Hero[];
+	shownHeroes: Hero[];
+	@Input() howManyHeroes: number = this.getHowManyHeroes();
 
-	ngOnInit() {}
+	getHowManyHeroes(): number {
+		let howManyHeroesStr: string = localStorage.getItem("howManyHeroesStr");
+
+		/** default case, if the localStorage method didn't get anything */
+		if (howManyHeroesStr === null || !isString(howManyHeroesStr) || howManyHeroesStr === "") {
+			howManyHeroesStr = "4"; // give default value & act the same as localStorage (return strings)
+		}
+
+		return Number(howManyHeroesStr);
+	}
+
+	constructor(private heroService: HeroService) {}
+
+	ngOnInit() {
+		this.getHeroes();
+	}
+
+	getHeroes(): void {
+		this.heroService.getHeroes().subscribe((heroes) => {
+			this.originalHeroes = heroes;
+			// this.originalHeroes = []; // testing
+			this.updateHowManyHeroesAreShowed();
+		});
+	}
+
+	updateHowManyHeroesAreShowed(): void {
+		console.log("Update Heroes", this.howManyHeroes);
+
+		if (isNumber(this.howManyHeroes)) {
+			this.shownHeroes = this.originalHeroes.slice(0, this.howManyHeroes);
+			localStorage.setItem("howManyHeroes", this.howManyHeroes.toString());
+		}
+	}
 }
