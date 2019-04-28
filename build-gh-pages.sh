f56f1c0840cd29826f23d578dc6679dc5fecbf9a
Kipras Melnikovas <kipras@kipras.org>
2019-04-28 23:49:22 +0300

    Add shebang & a check to ensure `npx` is installed
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+# build-gh-pages.sh
+
+! $(command -v npx >/dev/null 2>&1) && printf "Error! 'npx' not found. \nPlease install it. Exiting\n" && exit 1
+
 ng build --prod --output-path docs --base-href "https://sarpik.github.io/angular-learning/"
 printf "\nCommit message (or blank): "
 read commit_message
