780b1cf913c56e99aabdc4129e72044ae93c6e7b
Kipras Melnikovas <kipras@kipras.org>
2019-04-28 14:22:01 +0300

    First build, then ask for commit message @ build
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -1,4 +1,4 @@
+ng build --prod --output-path docs --base-href "https://sarpik.github.io/angular-learning/"
 printf "\nCommit message (or blank): "
 read commit_message
-ng build --prod --output-path docs --base-href "https://sarpik.github.io/angular-learning/"
 npx angular-cli-ghpages --dir=docs --message "${commit_message:-'Updating build for gh-pages'}"
