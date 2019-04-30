95d34965f14ad558c9007a281789491aa64f774a
Kipras Melnikovas <kipras@kipras.org>
2019-04-30 22:41:20 +0300

    Improve build-ghpages script: use `-m commit_msg`
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 # build-gh-pages.sh
+# use `-m <message>` to provide a custom commit message
 
 ! $(command -v npx >/dev/null 2>&1) && printf "Error! 'npx' not found. \nPlease install it. Exiting\n" && exit 1
 
+while getopts ":m:" option; do
+	case "${option}" in
+		m)
+			commit_message="${OPTARG}"
+	esac
+done
+
+# default case
+[ -z "$commit_message" ] && commit_message="Updating build for gh-pages"
+
+# build & publish
 ng build --prod --output-path docs --base-href "https://sarpik.github.io/angular-learning/"
-printf "\nCommit message (or blank): "
-read commit_message
-npx angular-cli-ghpages --dir=docs --message "${commit_message:-"Updating build for gh-pages"}"
+
+npx angular-cli-ghpages --dir=docs --message "$commit_message"
